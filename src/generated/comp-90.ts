ee1629120699f33fd2b4851f0b7e484c92874176
R-blaze Developer <dev@rblaze.com>
2025-12-30 00:37:56 +0100

    feat: add render to comp 90

--- src/generated/comp-90.ts
+++ src/generated/comp-90.ts
@@ -3,3 +3,4 @@ import React from 'react';
 import { memo } from 'react';
 export const Component90 = memo(() => {
   const [state, setState] = React.useState(0);
+  return React.createElement('div', null, 'Comp 90');
