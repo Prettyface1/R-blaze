284e10882c955c41fc088c26918d52bf5f5a88c6
R-blaze Developer <dev@rblaze.com>
2025-12-30 00:15:47 +0100

    feat: add state to comp 37

--- src/generated/comp-37.ts
+++ src/generated/comp-37.ts
@@ -9,3 +9,4 @@ export const Component37 = memo(() => {
 import React from 'react';
 import { memo } from 'react';
 export const Component37 = memo(() => {
+  const [state, setState] = React.useState(0);
