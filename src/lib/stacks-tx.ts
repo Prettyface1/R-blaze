9e0b4bc696acd1d84a8e849f78002ece56b1f992
R-blaze Developer <dev@rblaze.com>
2025-12-29 23:54:41 +0100

    feat: set target function name

--- src/lib/stacks-tx.ts
+++ src/lib/stacks-tx.ts
@@ -3,3 +3,5 @@ import { uintCV, PostConditionMode } from '@stacks/transactions';
 export const contributeToResearch = async (amount: number) => {
   await openContractCall({
     contractAddress: 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM',
+    contractName: 'R-blaze',
+    functionName: 'fund-project',
