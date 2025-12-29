2cac06986475051f896e3970b95d22d212f66ef9
R-blaze Developer <dev@rblaze.com>
2025-12-30 00:17:14 +0100

    feat: add render to comp 39

--- src/generated/comp-39.ts
+++ src/generated/comp-39.ts
@@ -9,3 +9,5 @@ export const Component39 = memo(() => {
 import React from 'react';
 import { memo } from 'react';
 export const Component39 = memo(() => {
+  const [state, setState] = React.useState(0);
+  return React.createElement('div', null, 'Comp 39');
