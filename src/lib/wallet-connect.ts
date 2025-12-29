dac1632298f731f7f3faa18e195ec65980fae71e
R-blaze Developer <dev@rblaze.com>
2025-12-29 23:55:10 +0100

    feat: configure wc provider metadata

--- src/lib/wallet-connect.ts
+++ src/lib/wallet-connect.ts
@@ -2,3 +2,7 @@ import { EthereumProvider } from '@walletconnect/ethereum-provider';
 const PROJECT_ID = 'your_project_id';
 export const initWalletConnect = async () => {
   const provider = await EthereumProvider.init({
+    projectId: PROJECT_ID,
+    showQrModal: true,
+    chains: [1],
+  });
