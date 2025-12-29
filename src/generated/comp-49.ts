1ccefda56759cb8d2ab06405d07b33450efd743d
R-blaze Developer <dev@rblaze.com>
2025-12-30 00:23:04 +0100

    feat: add react to comp 53

--- src/generated/comp-49.ts
+++ src/generated/comp-49.ts
@@ -8,3 +8,4 @@ export const Component49 = memo(() => {
 // Component: Atomic Component 49
 import React from 'react';
 import { memo } from 'react';
+export const Component49 = memo(() => {
--- src/generated/comp-53.ts
+++ src/generated/comp-53.ts
@@ -1,1 +1,2 @@
 // Component: Atomic Component 53
+import React from 'react';
