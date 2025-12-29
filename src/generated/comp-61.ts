1012742ca2eb2260b62c03dd878ea1a89e16dc24
R-blaze Developer <dev@rblaze.com>
2025-12-30 00:28:08 +0100

    test: setup test 65

--- src/generated/comp-61.ts
+++ src/generated/comp-61.ts
@@ -8,3 +8,5 @@ export const Component61 = memo(() => {
 // Component: Atomic Component 61
 import React from 'react';
 import { memo } from 'react';
+export const Component61 = memo(() => {
+  const [state, setState] = React.useState(0);
