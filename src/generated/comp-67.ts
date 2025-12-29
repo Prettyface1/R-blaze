7f51616ca15bedb9dffd420971dea55a3e97df43
R-blaze Developer <dev@rblaze.com>
2025-12-30 00:30:28 +0100

    feat: add react to comp 71

--- src/generated/comp-67.ts
+++ src/generated/comp-67.ts
@@ -5,3 +5,4 @@ export const Component67 = memo(() => {
   const [state, setState] = React.useState(0);
   return React.createElement('div', null, 'Comp 67');
 });
+// Component: Atomic Component 67
--- src/generated/comp-71.ts
+++ src/generated/comp-71.ts
@@ -1,1 +1,2 @@
 // Component: Atomic Component 71
+import React from 'react';
