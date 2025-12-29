c94f1a20d59600b166a9f0bff3df4880723077ca
R-blaze Developer <dev@rblaze.com>
2025-12-30 00:35:55 +0100

    feat: add memo to comp 85

--- src/generated/comp-81.ts
+++ src/generated/comp-81.ts
@@ -8,3 +8,4 @@ export const Component81 = memo(() => {
 // Component: Atomic Component 81
 import React from 'react';
 import { memo } from 'react';
+export const Component81 = memo(() => {
--- src/generated/comp-85.ts
+++ src/generated/comp-85.ts
@@ -1,2 +1,3 @@
 // Component: Atomic Component 85
 import React from 'react';
+import { memo } from 'react';
