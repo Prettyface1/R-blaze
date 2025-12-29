e522c381e8887a898e7abdbf6c01ff3e4653ffd9
R-blaze Developer <dev@rblaze.com>
2025-12-30 00:34:06 +0100

    docs: init doc 80

--- src/generated/comp-76.ts
+++ src/generated/comp-76.ts
@@ -9,3 +9,4 @@ export const Component76 = memo(() => {
 import React from 'react';
 import { memo } from 'react';
 export const Component76 = memo(() => {
+  const [state, setState] = React.useState(0);
