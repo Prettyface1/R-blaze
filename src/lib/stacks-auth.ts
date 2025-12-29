8d832bd31e83f219a8f356205878147e48e7f6d9
R-blaze Developer <dev@rblaze.com>
2025-12-29 23:54:16 +0100

    feat: add app details to connect modal

--- src/lib/stacks-auth.ts
+++ src/lib/stacks-auth.ts
@@ -2,3 +2,5 @@ import { AppConfig, UserSession, showConnect } from '@stacks/connect';
 const appConfig = new AppConfig(['store_write', 'publish_data']);
 export const userSession = new UserSession({ appConfig });
 export const authenticate = () => {
+  showConnect({
+    appDetails: { name: 'R-blaze', icon: '/logo.png' },
